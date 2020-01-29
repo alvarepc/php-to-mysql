3f7b74322a5418a381eb17857444d379b95afcd3
alvarepc <55838813+alvarepc@users.noreply.github.com>
2020-01-29 19:39:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -20,28 +20,28 @@ if ($connection->connect_errno) {
 } else {
     printf("<br><br>Connected to the database");
 	# Display the table
-	$query = "SELECT * FROM instructors";
+	$query = "SELECT * FROM instructors;";
 	if ($result = $connection->query($query)) {
 		echo '<table border="0" cellspacing="2" cellpadding="2"> 
 		<tr> 
-			<td> <font face="Arial">instructorNumber</font> </td> 
-			<td> <font face="Arial">instructorName</font> </td> 
-			<td> <font face="Arial">email</font> </td> 
+			<td> <font face="Arial">Instructor Number</font> </td> 
+			<td> <font face="Arial">Instructor Name</font> </td> 
+			<td> <font face="Arial">Email</font> </td> 
 			<td> <font face="Arial">city</font> </td> 
-			<td> <font face="Arial">state</font> </td> 
-			<td> <font face="Arial">postalCode</font> </td> 
-			<td> <font face="Arial">country</font> </td> 
+			<td> <font face="Arial">State</font> </td> 
+			<td> <font face="Arial">Postal Code</font> </td> 
+			<td> <font face="Arial">Country</font> </td> 
 		</tr>';
 		/* fetch associative array */
 		while ($row = $result->fetch_assoc()) {
 			echo '<tr> 
-					<td>'.$row["col1"].'</td> 
-					<td>'.$row["col2"].'</td> 
-					<td>'.$row["col3"].'</td> 
-					<td>'.$row["col4"].'</td> 
-					<td>'.$row["col5"].'</td> 
-					<td>'.$row["col6"].'</td> 
-					<td>'.$row["col7"].'</td> 
+					<td>'.$row["instructorNumber"].'</td> 
+					<td>'.$row["instructorName"].'</td> 
+					<td>'.$row["email"].'</td> 
+					<td>'.$row["city"].'</td> 
+					<td>'.$row["state"].'</td> 
+					<td>'.$row["postalCode"].'</td> 
+					<td>'.$row["country"].'</td> 
 				  </tr>';
 		}
 		echo '</table>';
